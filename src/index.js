99f83afb87570758fca8314d524f3a750e8a26c7
faith-kerry <kerubofaith944@gmail.com>
2025-09-30 23:46:05 +0300

    completed the Task-lister Mini-Project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector("#create-task-form"); // grab the form
+
+  form.addEventListener("submit", function(e) {
+    e.preventDefault(); // stop page refresh
+
+    const input = document.querySelector("#new-task-description"); // the input
+    const taskList = document.querySelector("#tasks"); // the ul
+
+    const li = document.createElement("li");
+    li.textContent = input.value; // set text to the task description
+    taskList.appendChild(li); // add it to the list
+
+    input.value = ""; // clear input field
+  });
 });
